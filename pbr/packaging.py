02a5355418f5f5a7b2f3008b04789a56f24b2f4d
arun007007 <aruncse2007007@gmail.com>
2015-06-09 17:48:44 +0600

    delving into LocalEggInfo and LocalManifestMaker class

--- pbr/packaging.py
+++ pbr/packaging.py
@@ -14,12 +14,13 @@ from setuptools.command import sdist
 
 class LocalInstall(install.install):
     command_name = 'install'
+    print "THE PROGRAM HAS ENTERED INTO LOCALINSTALL CLASS. "
     def run(self):
         return du_install.install.run(self)
 
 class LocalInstallScripts(install_scripts.install_scripts):
     command_name = 'install_scripts'
-    print "The program has entered into LocalInstallScripts: "
+    print "THE PROGRAM HAS ENTERED INTO LOCALINSTALLSCRIPT CLASS"
     def run(self):
         import distutils.command.install_scripts
 
@@ -48,35 +49,40 @@ class LocalInstallScripts(install_scripts.install_scripts):
             self.write_script(*args)
 
 class LocalManifestMaker(egg_info.manifest_maker):
+    print "THE PROGRAM HAS ENTERED INTO LOCALMANIFESTMAKER CLASS."
     def add_defaults(self):
         sdist.sdist.add_defaults(self)
         self.filelist.append(self.template)
         self.filelist.append(self.manifest)
         ei_cmd = self.get_finalized_command('egg_info')
+        print "LOCALMANIFESTMAKER [ei_cmd]: ", ei_cmd
         self.filelist.include_pattern("*", prefix=ei_cmd.egg_info)
 
 
-class LocalEggInfo(egg_info.egg_info):
+class LocalEggInfo(egg_info.egg_info): # It is needed in the sdist command, not in install. 
     command_name = 'egg_info'
-    print "The program has entered into LocalEggInfo: "
+    print "PROGRAM HAS ENTERED INTO LOCALEGGINFO CLASS. : "
     def find_sources(self):
         manifest_filename = os.path.join(self.egg_info, "SOURCES.txt")
-        print "manifest_filename: ", manifest_filename
+        print "LOCALEGGINFO[manifest_filename]: ", manifest_filename
         if (not os.path.exists(manifest_filename) or 'sdist' in sys.argv):
             mm = LocalManifestMaker(self.distribution)
-            print "mm : ", mm
+            print "LOCALEGGINFO[mm] : ", mm
             mm.manifest = manifest_filename
+            print "LOCALEGGINFO[mm.manifest]: ", mm.manifest
             mm.run()
             self.filelist = mm.filelist
+            print "LOCALEGGINFO [self.filelist]: ", self.filelist
         else:
             self.filelist = egg_info.FileList()
-            print "self.filelist: ", self.filelist
+            print "LOCALEGGINFO [self.filelist]: ", self.filelist
             for entry in open(manifest_filename, 'r').read().split('\n'):
                 self.filelist.append(entry)
 
 
 class LocalSDist(sdist.sdist):
     command_name = 'sdist'
+    print "PROGRAM HAS ENTERED INTO LOCALSDIST class."
     def run(self):
         print "This is the LocalSDist Run Method`"
         sdist.sdist.run(self)
